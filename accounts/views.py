290ea9a7cd1d1aedc86011f31591b8a84d5e7c66
byron lin <byronlin92@gmail.com>
2018-01-14 00:32:19 -0800

    updated login/ logout . added account details

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import SignUpForm
+from django.contrib.auth.models import User
+
+#login decorators
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 
@@ -9,8 +14,13 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request,user)
+            login(request, user)
             return redirect('home')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form':form})
+
+@login_required
+def account_detail(request, user_username):
+    user = User.objects.get(username=user_username)
+    return render(request, 'account_detail.html', {'user': user})
