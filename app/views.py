54d8717b993787621a0a2f9759bdf24f321fbdf7
byron lin <byronlin92@gmail.com>
2018-01-13 19:28:23 -0800

    added post detail

--- app/views.py
+++ app/views.py
@@ -10,13 +10,14 @@ def posts(request):
     posts = Post.objects.all()
     return render(request, 'posts.html', {'posts':posts})
 
-def posts_new(request):
+def post_new(request):
     if request.method == 'POST':
         form = NewPostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             # post.created_by = None
             post.created_at = timezone.now()
+            post.updated_at = timezone.now()
             post.save()
             return redirect('posts')
     else:
@@ -24,3 +25,20 @@ def posts_new(request):
     return render(request, 'post_new.html', {'form': form})
 
 
+def post_detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    return render(request, 'post_detail.html', {'post': post})
+
+def post_update(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.method == 'POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # post.created_by = None
+            post.updated_at = timezone.now()
+            post.save()
+            return redirect('post_detail', {'post':post})
+    else:
+        form = NewPostForm()
+    return render(request, 'post_update.html', {'post': post})
