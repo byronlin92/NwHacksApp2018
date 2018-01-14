06c620e64165287b37447bf66c3f84330b9d0559
mohitkewalramani7 <mohitkewalramani74@gmail.com>
2018-01-14 03:47:39 -0800

    Fixed merge conflicts

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,39 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('first_name', 'last_name','username', 'email', 'password1', 'password2')
+
+
+class AccountUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
+
+class UpdatePasswordForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput())
+    confirm_password=forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ['password',]
+
+    def clean(self):
+        cleaned_data = super(UpdatePasswordForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio']
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,17 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+from django.contrib.auth import login
+from .forms import SignUpForm, AccountUpdateForm, UpdatePasswordForm, ProfileUpdateForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
+
 from django.contrib.auth.models import User
-
-#login decorators
+from .models import Profile
+#login decorator
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 
 
+#ACCOUNTS
 def signin(request):
     email = request.POST['emailAddress']
     password = request.POST['password']
@@ -33,3 +37,46 @@ def signup(request):
 def account_detail(request, user_username):
     user = User.objects.get(username=user_username)
     return render(request, 'account_detail.html', {'user': user})
+
+
+@login_required
+def account_update(request, user_username):
+    user = get_object_or_404(User, username=user_username)
+    if request.method == 'POST':
+        form = AccountUpdateForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save()
+            return redirect('account_detail', user_username=user_username)
+    else:
+        form = AccountUpdateForm()
+    return render(request, 'account_update.html', {'user':user, 'form':form})
+
+
+#PROFILES
+def profile_detail(request, profile_pk):
+    profile = get_object_or_404(Profile, pk=profile_pk)
+    return render(request, 'profile_detail.html', {'profile':profile})
+
+@login_required
+def profile_update(request, profile_pk):
+    profile = get_object_or_404(Profile, pk=profile_pk)
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=profile)
+        if form.is_valid():
+            profile = form.save()
+            return redirect('profile_detail', profile_pk=profile_pk)
+    else:
+        form = ProfileUpdateForm()
+    return render(request, 'profile_update.html', {'profile':profile, 'form':form})
+
+@login_required
+def password_change(request, user_username):
+    user = get_object_or_404(User, username=user_username)
+    if request.method == 'POST':
+        form = UpdatePasswordForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save()
+            return redirect('account_detail', user_username=user_username)
+    else:
+        form = UpdatePasswordForm()
+    return render(request, 'password_update.html', {'user':user, 'form':form})
